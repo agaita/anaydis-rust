2a33a642cb2b0a9fa8dc71598d8b3ebf66635306
Alvaro Gaita <alvaro.gaita@mulesoft.com>
2020-08-23 19:29:12 -0300

    feat: adding a simple gessing game

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use std::io;
+use rand::{thread_rng, Rng};
+use std::cmp::Ordering;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    game();
+}
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+fn game() {
+    println!("Guess the number!");
+
+    let secret_number = thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
